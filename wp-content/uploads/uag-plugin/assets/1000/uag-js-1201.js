8f7792f741bef0e3b5f0ecc5cb33f95286b12c84
Daryl Jane Luz <119088227+Djluz@users.noreply.github.com>
2025-10-20 18:25:13 +0800

    Add files via upload

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1201.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBTimelineClasses( {"block_id":"64b90f7c","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-64b90f7c' );
+});
+window.addEventListener("resize", function(){
+	UAGBTimelineClasses( {"block_id":"64b90f7c","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-64b90f7c' );
+});
+window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-db9f3d71 .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"flip","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-db9f3d71 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-db9f3d71 .swiper-button-next","prevEl":".uagb-block-db9f3d71 .swiper-button-prev"}}	);
+});
+
+ });
---
+++ wp-content/uploads/uag-plugin/assets/3000/uag-js-2646.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-7de08e7e' );
+	UAGBTabs.anchorTabId( '.uagb-block-7de08e7e' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-7de08e7e' );
+}, false );
+ });
